d455542794e8dd86636e64728304428257b7d701
pabascom <phillip.bascom@mobileappscompany.us>
2019-05-05 09:34:06 -0400

    last commit before fragmentation

--- app/src/main/java/phil/homework/week1day5_navigationdrawer/MainActivity.kt
+++ app/src/main/java/phil/homework/week1day5_navigationdrawer/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.design.widget.NavigationView
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 
@@ -13,28 +15,38 @@ class MainActivity : AppCompatActivity() {
     lateinit var dl: DrawerLayout
     lateinit var t: ActionBarDrawerToggle
     lateinit var nv: NavigationView
+    lateinit var tb: android.support.v7.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tb = findViewById(R.id.toolbar)
+        setSupportActionBar(tb)
 
         dl = findViewById(R.id.activity_main)
         t = ActionBarDrawerToggle(this, dl, R.string.open, R.string.close)
         dl.addDrawerListener(t)
         t.syncState()
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         nv = findViewById(R.id.nv)
         nv.setNavigationItemSelectedListener { item: MenuItem ->
             when (item.itemId) {
-                R.id.account -> Toast.makeText(this@MainActivity, "My Account", Toast.LENGTH_SHORT).show()
-                R.id.settings -> Toast.makeText(this@MainActivity, "Settings", Toast.LENGTH_SHORT).show()
-                R.id.mycart -> Toast.makeText(this@MainActivity, "My Cart", Toast.LENGTH_SHORT).show()
+                R.id.contacts -> Toast.makeText(this@MainActivity, "Search Contacts", Toast.LENGTH_SHORT).show()
+                R.id.flashlight -> Toast.makeText(this@MainActivity, "Use Flashlight", Toast.LENGTH_SHORT).show()
+                R.id.emi -> Toast.makeText(this@MainActivity, "Calculate EMI", Toast.LENGTH_SHORT).show()
                 else -> true
             }
             true
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
