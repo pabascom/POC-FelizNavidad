1862f2f24e31a5cd1430c9af284762f765d1f306
pabascom <phillip.bascom@mobileappscompany.us>
2019-05-05 14:46:13 -0400

    semifunctional Search View, nonfunctional Flashlight

---
+++ app/src/main/java/phil/homework/week1day5_navigationdrawer/Flashlight.kt
@@ -0,0 +1,22 @@
+package phil.homework.week1day5_navigationdrawer
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_flashlight.*
+
+class Flashlight : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flashlight)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
---
+++ app/src/main/java/phil/homework/week1day5_navigationdrawer/SearchableActivity.kt
@@ -0,0 +1,12 @@
+package phil.homework.week1day5_navigationdrawer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchableView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchable_view)
+    }
+}
